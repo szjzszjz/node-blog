6e1c2446ce8a5e8fbbae5074339ba3cd4120e5d9
史博 <312685889@qq.com>
2019-09-01 14:57:24 +0800

    添加注册功能

--- controller/user.js
+++ controller/user.js
@@ -1,5 +1,5 @@
 const { exec, escape } = require('../db/mysql')
-const { ErrorModel } = require('../model/resModel')
+const { ErrorModel , SuccessModel} = require('../model/resModel')
 const { genPassword } = require('../utils/cryp')
 
 
@@ -9,7 +9,7 @@ const login = async (username, password) => {
 
     //利用escape防止sql注入
     password = genPassword(password)
-    console.log('password=', password);
+    console.log('login password=', password);
 
     username = escape(username)
     password = escape(password)
@@ -31,6 +31,34 @@ const login = async (username, password) => {
     // })
 }
 
+const register = async (username, password) => {
+    //利用escape防止sql注入
+    password = genPassword(password)
+    console.log('register password=', password);
+
+    username = escape(username)
+    password = escape(password)
+
+    //判断用户是否已存在
+    const justsql = `select username from user where username = ${username}`
+    const justResult = await exec(justsql)
+    console.log('justResult=', justResult);
+    if (justResult !== null && justResult.length > 0) {
+        return new ErrorModel('该用户名已经存在!')
+    }
+
+    const sql = `insert into user(username, password, realname) values(${username}, ${password}, ${username});`
+    const result = await exec(sql)
+    console.log('sql=', sql, result['affectedRows']);
+    if (result['affectedRows'] > 0) {
+        return new SuccessModel('注册成功')
+    }else{
+        return new ErrorModel('注册失败')
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    register
 }
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')()
-const { login } = require('../controller/user')
+const { login,register } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 router.prefix('/api/user')
@@ -18,15 +18,11 @@ router.post('/login', async function (ctx, next) {
     }
 })
 
-// router.get('/session-test', async function (ctx, next) {
-//     if (ctx.session.viewCount == null) {
-//         ctx.session.viewCount = 0
-//     }
-//     ctx.session.viewCount++
-//     ctx.body = {
-//         errno: 0,
-//         viewCount: ctx.session.viewCount
-//     }
-// })
+router.post('/register', async function (ctx, next) {
+    const { username, password } = ctx.request.body
+    const result = await register(username, password)
+    console.log('register result = ', result);
+    ctx.body = result
+})
 
 module.exports = router
